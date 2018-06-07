cc2214794deb503d6b53cc7f5df4fd6b80e217bc
mdotbollinger <39972673+mdotbollinger@users.noreply.github.com>
2018-06-07 11:33:52 -0400

    upload complete?

---
+++ Grand Circus/Lab1 P2/script_ex1.js
@@ -0,0 +1,8 @@
+console.log("enter a number:");
+var response = prompt("please enter a number","please enter a number greater than");
+var i;
+var total=0;
+for (i = 1;i<=response; i++){
+  total=total+i;
+}
+console.log("output: "+total)
---
+++ Grand Circus/Lab1 P2/script_ex2.js
@@ -0,0 +1,8 @@
+var response = prompt("Do you want to play");
+if (yes) document.write("Enter a word");
+var response = prompt("Enter a word");
+var total=0;
+for (i = 1;i<=response; i++){
+  total=total+i;
+}
+console.log("output: "+total)
